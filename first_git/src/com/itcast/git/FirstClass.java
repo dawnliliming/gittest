db18b91801e0b300326b50c51e8390e4b7febaad
dawnli <3358115621@qq.com>
2020-04-05 13:53:19 +0800

    fristgit工程

---
+++ first_git/src/com/itcast/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.itcast.git;
+
+/**
+ * Created by ASUS on 2020/4/5.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
